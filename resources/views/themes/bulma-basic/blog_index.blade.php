6e18111b126f84341ec641790c42c4e0e8d1df89
Antoni Putra <akiddcode@gmail.com>
2017-12-04 06:43:09 +0700

    delete unused theme

--- resources/views/themes/bulma-basic/blog_index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('ngeblog_public::layout')
-
-@section('content')
-    @forelse (Ngeblog::getLatestBlog(5) as $b)
-        @include('ngeblog_public::_blog_list', ['blog' => $b])
-    @empty
-    	<h4>There is no Blog yet.</h4>
-    @endforelse
-@stop
--- resources/views/themes/bulma-basic/category_show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('ngeblog_public::layout')
-
-@section('content')
-    @foreach ($category['blogs'] as $b)
-        @include('ngeblog_public::_blog_list', ['blog' => $b])
-    @endforeach
-@stop
