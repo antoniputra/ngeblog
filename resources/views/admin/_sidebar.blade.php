ce5f8e9240767ca09f3bac2dc02fb72e74e81b3f
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-12-08 10:17:38 +0700

    Add Post Meta menu on sidebar

--- resources/views/admin/_sidebar.blade.php
+++ resources/views/admin/_sidebar.blade.php
@@ -24,6 +24,13 @@
 			</span>
 			Blog
 		</a></li>
+		<li><a href="{{ route('ngeblog.postmeta.index') }}"
+			@if(str_contains(Route::currentRouteName(), '.blog')) class="is-active" @endif>
+			<span class="icon is-medium">
+				<i class="ion-gear-b"></i>
+			</span>
+			Post Meta
+		</a></li>
 		{{-- <li><a href="#">
 			<span class="icon is-medium">
 				<i class="ion-ios-alarm-outline"></i>
