f9d7805a9d449d9ee0de5eac97532ef794138ef9
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-12-10 09:05:24 +0700

    Add dropdown-ajax library to show postmeta by selected dropdown

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,37 +1,36 @@
 // setup lodash
-window._ = require('lodash');
-
+window._ = require("lodash");
 
 // setup jquery
 try {
-    // window.$ = window.jQuery = require('jquery');
+  // window.$ = window.jQuery = require('jquery');
 } catch (e) {}
 
-
 // setup axios
-window.axios = require('axios');
+window.axios = require("axios");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 // setup token
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+  window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+  console.error(
+    "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
+  );
 }
 
 // Object.defineProperty(Vue.prototype, '$lodash', { value: lodash });
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
-Vue.component('markdown', require('./components/Markdown.vue'));
+Vue.component("markdown", require("./components/Markdown.vue"));
 
 const app = new Vue({
-    el: '#app'
+  el: "#app"
 });
 
-
-require('./link-method.js')
+require("./link-method");
+require("./dropdown-ajax");
