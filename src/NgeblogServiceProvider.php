4a344b2f284a9ef81a727b4d8e3f12d23ea9cacc
antoniputra <akiddcode@gmail.com>
2024-04-07 00:34:21 +0700

    Package Setup and Testbench

---
+++ src/NgeblogServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AntoniPutra\Ngeblog;
+
+use Illuminate\Support\ServiceProvider;
+
+final class NgeblogServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+
+    }
+}
